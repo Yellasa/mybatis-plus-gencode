ced7b818c62f5e638b7245bad10d451c094b5178
码哥 <yangting@farpower.com>
2018-07-12 11:06:56 +0800

    INIT
    1、mybatis-plus集成springboot
    2、添加mybatis-plus代码生成器

---
+++ src/test/java/com/baomidou/springboot/test/GeneratorServiceEntity.java
@@ -0,0 +1,61 @@
+package com.baomidou.springboot.test;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.junit.Test;
+
+public class GeneratorServiceEntity {
+
+    @Test
+    public void generateCode() {
+        String packageName = "com.baomidou.springboot";
+        boolean serviceNameStartWithI = false;//user -> UserService, 设置成true: user -> IUserService
+        //generateByTables(serviceNameStartWithI, packageName, "fp_bank_info", "role");
+        generateByTables(serviceNameStartWithI, packageName, "fp_bank_info");
+    }
+
+    private void generateByTables(boolean serviceNameStartWithI, String packageName, String... tableNames) {
+        GlobalConfig config = new GlobalConfig();
+        String dbUrl = "jdbc:mysql://localhost:3306/test?serverTimezone=UTC";
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL)
+                .setUrl(dbUrl)
+                .setUsername("root")
+                .setPassword("123456")
+                .setDriverName("com.mysql.jdbc.Driver");
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig
+                .setCapitalMode(true)
+                .setEntityLombokModel(false)
+                .setDbColumnUnderline(true)
+                .setNaming(NamingStrategy.underline_to_camel)
+                .setTablePrefix("fp")//去掉表名前缀  TEST_USER -> User.java
+                //.fieldPrefix("test")//去掉字段前缀   test_id -> id, test_type -> type
+                .setInclude(tableNames);//修改替换成你需要的表名，多个表名传数组
+        config.setActiveRecord(false)
+                .setAuthor("yangting")
+                .setOutputDir("e:\\codeGen")
+                .setFileOverride(true);
+        if (!serviceNameStartWithI) {
+            config.setServiceName("%sService");
+        }
+        new AutoGenerator().setGlobalConfig(config)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(
+                        new PackageConfig()
+                                .setParent(packageName)
+                                .setController("controller")
+                                .setEntity("entity")
+                ).execute();
+    }
+
+    private void generateByTables(String packageName, String... tableNames) {
+        generateByTables(true, packageName, tableNames);
+    }
+}
